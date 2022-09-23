e23f11975a759736b412c20a80335fafad6bc3c9
Crapteep <crapteep@gmail.com>
2022-09-23 22:01:01 +0200

    created new functions for calculate price and get datetime with a special format

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 
 def date_extract(notification_time):
     try:
@@ -9,3 +9,16 @@ def date_extract(notification_time):
     dd, mm, rrrr = new_date.strip().split('.')
     final_date = f'{rrrr}-{mm}-{dd} {new_time.strip()}'
     return final_date
+
+
+def calc_percent_of_price(price, percent):
+    return round(price - price * (percent / 100), 1)
+
+
+def calc_price_before_discount(discounted_price, discount_percent):
+    return int(discounted_price / ((100 - discount_percent) / 100))
+
+
+def get_today_date():
+    time_now = datetime.now()
+    return time_now.strftime("%Y-%m-%d %H:%M:%S")
